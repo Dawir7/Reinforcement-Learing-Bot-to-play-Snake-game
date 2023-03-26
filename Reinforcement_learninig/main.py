2f8a2e4154ec6a9d0c566325f8d5d33723b9bb66
Dawir7 <dawir@tlen.pl>
2023-03-26 14:35:37 +0200

    Model Alfa version.

--- Reinforcement_learninig/main.py
+++ Reinforcement_learninig/main.py
@@ -14,28 +14,6 @@ def redraw_window(win: pygame.display.set_mode, snake: Snake, playground: Map):
     pygame.display.update()  # This updates the screen so we can see our rectangle
 
 
-# def main():
-#     playground = Map()
-#     win = pygame.display.set_mode((playground.map_size, playground.map_size))
-#
-#     snake = Snake()
-#     run = True
-#     clock = pygame.time.Clock()
-#     while run:
-#         # ToDo: Time management.
-#         clock.tick(30)
-#         pygame.time.delay(100)
-#         snake.keyboard_input()
-#         playground.random_snack_pos(snake)
-#
-#         if snake.collision(playground):
-#             print(f"GAME OVER\nSCORE: {playground.score}")
-#             run = False
-#             # game_over(win, playground, snake)
-#             break
-#         redraw_window(win, snake, playground)
-
-
 def main():
     # MODEL
     q_table = np.zeros((2 ** 11, 3))
